ff420e2d794444b05c97be4fffdc62c88367e587
mukesh <22us25@anjaconline.org>
2025-03-08 19:28:49 +0530

    first commit

--- components/guest.py
+++ components/guest.py
@@ -222,7 +222,7 @@ def guest_page():
             # Query LM Studio for the answer
             
                 
-            result_text = genai.lama.query_lm_studio(question,data_feed_back if data_feed_back else context)          
+            result_text = genai.lama.query_lm_studio(question,context)          
             # Store the question and answer in session state
             st.session_state.qa_list.append({'question': question, 'answer': result_text})
 
